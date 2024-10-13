dd248ae3af89009394aa190413dfc81f71194b27
isayaksh <isayaksh@gmail.com>
2024-10-13 21:32:36 +0900

    <init> spring boot setting

---
+++ src/test/java/com/oe/OurEatsAPI/OurEatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oe.OurEatsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurEatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
